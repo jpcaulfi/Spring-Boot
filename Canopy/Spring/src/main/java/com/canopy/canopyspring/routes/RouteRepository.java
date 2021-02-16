832d69010f292db3aaab6efb5c2cad53f8fb5ec6
Joseph Caulfield <joecaulfield29@yahoo.com>
2021-02-15 21:04:40 -0500

    Delete Canopy/Spring directory

--- Canopy/Spring/src/main/java/com/canopy/canopyspring/routes/RouteRepository.java
+++
@@ -1,8 +0,0 @@
-package com.canopy.canopyspring.routes;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RouteRepository extends JpaRepository<Route, Long> {
-}
