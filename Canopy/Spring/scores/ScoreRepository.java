f0348ac3dd8a092987b5c2e80a525aa9426af483
Joseph Caulfield <joecaulfield29@yahoo.com>
2021-02-16 15:27:05 -0500

    Delete Canopy directory

--- Canopy/Spring/scores/ScoreRepository.java
+++
@@ -1,8 +0,0 @@
-package com.canopy.canopyspring.scores;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ScoreRepository extends JpaRepository<Score, Long> {
-}
