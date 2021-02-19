237209393da9ff14880e22f241fdc63afefd4d39
Joseph Caulfield <joecaulfield29@yahoo.com>
2021-02-18 23:00:02 -0500

    Delete Canopy directory

--- Canopy/Spring/rescues/RescueRepository.java
+++
@@ -1,8 +0,0 @@
-package com.canopy.canopyspring.rescues;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RescueRepository extends JpaRepository<Rescue, Long> {
-}
